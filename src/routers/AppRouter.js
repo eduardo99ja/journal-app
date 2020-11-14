ae7c896985ea2fe9074853c8c0c919194ef972b1
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-14 10:31:35 -0600

    Keep the application state when reloading

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
-  } from 'react-router-dom';
-
-import { AuthRouter } from './AuthRouter';
-import { JournalScreen } from '../components/journal/JournalScreen';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import { firebase } from '../firebase/firebase-config'
+import { AuthRouter } from './AuthRouter'
+import { JournalScreen } from '../components/journal/JournalScreen'
+import { useDispatch } from 'react-redux'
+import { login } from '../actions/auth'
 
 export const AppRouter = () => {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route 
-                        path="/auth"
-                        component={ AuthRouter }
-                    />
+  const dispatch = useDispatch()
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      //if user object exists, evaluate uid
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName))
+      }
+    })
+  }, [dispatch])
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/auth' component={AuthRouter} />
 
-                    <Route 
-                        exact
-                        path="/"
-                        component={ JournalScreen }
-                    />
+          <Route exact path='/' component={JournalScreen} />
 
-                    <Redirect to="/auth/login" />
-
-
-                </Switch>
-            </div>
-        </Router>
-    )
+          <Redirect to='/auth/login' />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
