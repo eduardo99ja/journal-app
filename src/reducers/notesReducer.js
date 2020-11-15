3c4ef5ca61c6b0e19333fa2f25127a1fc2a1ff7b
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-15 16:56:47 -0600

    getting notes from firestore

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,22 +1,25 @@
-import { types } from "../types/types"
+import { types } from '../types/types'
 
-
-
-const initialState ={
-    notes:[],
-    active:null
+const initialState = {
+  notes: [],
+  active: null,
 }
-export const notesReducer=(state=initialState, action)=>{
-    switch (action.type) {
-        case types.noteActive:
-        return{
-            ...state,
-            active:{
-                ...action.payload
-            }
-        }
-    
-        default:
-            return state
-    }
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.noteActive:
+      return {
+        ...state,
+        active: {
+          ...action.payload,
+        },
+      }
+    case types.notesLoad:
+      return {
+        ...state,
+        notes: [...action.payload],
+      }
+
+    default:
+      return state
+  }
 }
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,17 +7,22 @@ import { useDispatch } from 'react-redux'
 import { login } from '../actions/auth'
 import { PrivateRoute } from './PrivateRoute'
 import { PublicRoute } from './PublicRoute'
+import { loadNotes } from '../helpers/loadNotes'
+import { setNotes } from '../actions/notes'
 
 export const AppRouter = () => {
   const dispatch = useDispatch()
   const [checking, setChecking] = useState(true)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged(async user => {
       //if user object exists, evaluate uid
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName))
         setIsLoggedIn(true)
+
+        const notes = await loadNotes(user.uid)
+        dispatch(setNotes(notes))
       } else {
         setIsLoggedIn(false)
       }
