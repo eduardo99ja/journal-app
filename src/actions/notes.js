5cba04bcd2208a9e5f50b125f7b1d4ca25d20dac
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-15 21:40:14 -0600

    Update the url of the active note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -67,12 +67,20 @@ export const refreshNote = (id, note) => ({
   },
 })
 
-
-
-export const startUploading =(file)=>{
-    return async(dispatch,getState)=>{
-        const {active:activeNote}= getState().notes
-        const fileUrl = await fileUpload(file)
-        console.log(fileUrl);
-    }
+export const startUploading = file => {
+  return async (dispatch, getState) => {
+    const { active: activeNote } = getState().notes
+    Swal.fire({
+      title: 'Uploading....',
+      text: 'Please wait...',
+      allowOutsideClick: false,
+      onBeforeOpen: () => {
+        Swal.showLoading()
+      },
+    });
+    const fileUrl = await fileUpload(file)
+    activeNote.url=fileUrl
+    dispatch(startSaveNote(activeNote))
+    Swal.close()
+  }
 }
